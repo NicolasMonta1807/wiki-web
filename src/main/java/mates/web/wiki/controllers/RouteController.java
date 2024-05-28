a11b2013de5d002b8a587795a68d9b7476afc207
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-05-28 14:39:41 -0500

    feat: contact form send emails

--- src/main/java/mates/web/wiki/controllers/RouteController.java
+++ src/main/java/mates/web/wiki/controllers/RouteController.java
@@ -1,5 +1,6 @@
 package mates.web.wiki.controllers;
 
+import mates.web.wiki.services.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -16,36 +17,88 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/grupo-1-1-wiki")
 public class RouteController {
 
-  @Autowired
-  private ContactRepository contactRepository;
+    private final ContactRepository contactRepository;
+    private final MailService mailService;
 
-  @GetMapping(value = {"/", ""})
-  public ModelAndView getHomePage() {
-    return new ModelAndView("home");
-  }
-
-  @GetMapping("/contact")
-  public ModelAndView getContactPage() {
-    ModelAndView modelAndView = new ModelAndView("contact");
-    modelAndView.addObject("contact", new Contact());
-    return modelAndView;
-  }
-
-  @GetMapping("/project")
-  public ModelAndView getProjectPage() {
-    return new ModelAndView("project");
-  }
-
-  @PostMapping("/form/contact")
-  public ModelAndView postContactForm(@Valid Contact contact, BindingResult bindingResult, ModelAndView modelAndView) {
-    if (bindingResult.hasErrors()) {
-      modelAndView.setViewName("contact");
-      modelAndView.addObject("contact", contact);
-      return modelAndView;
+    RouteController(ContactRepository contactRepository, MailService mailService) {
+        this.contactRepository = contactRepository;
+        this.mailService = mailService;
     }
 
-    contactRepository.save(contact);
-    return new ModelAndView("home");
-  }
+    @GetMapping(value = {"/", ""})
+    public ModelAndView getHomePage() {
+        return new ModelAndView("home");
+    }
+
+    @GetMapping("/contact")
+    public ModelAndView getContactPage() {
+        ModelAndView modelAndView = new ModelAndView("contact");
+        modelAndView.addObject("contact", new Contact());
+        return modelAndView;
+    }
+
+    @GetMapping("/project")
+    public ModelAndView getProjectPage() {
+        return new ModelAndView("project");
+    }
+
+    @PostMapping("/form/contact")
+    public ModelAndView postContactForm(@Valid Contact contact, BindingResult bindingResult, ModelAndView modelAndView) {
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("contact");
+            modelAndView.addObject("contact", contact);
+            return modelAndView;
+        }
+
+        sendConfirmationEmails(contact);
+        contactRepository.save(contact);
+        return new ModelAndView("home");
+    }
+
+    private void sendConfirmationEmails(Contact contact) {
+        mailService.sendEmail(
+                contact.getEmail(),
+                "Thank you for contacting us - TwoMates",
+                getContentToUser(contact),
+                true
+        );
+        mailService.sendEmail(
+                "naingmd@proton.me",
+                "An user has sent a message - TwoMates",
+                getContentToCrew(contact),
+                true
+        );
+        mailService.sendEmail(
+                "nicolasmonta1807@gmail.com",
+                "An user has sent a message - TwoMates",
+                getContentToCrew(contact),
+                true
+        );
+    }
+
+    private static String getContentToUser(Contact contact) {
+        String contentToUser = "Dear [[name]],<br>"
+                + "Thank you for contacting us. Here's a copy of your message<br>"
+                + "<p>[[message]]</p>"
+                + "Thank you.<br>"
+                + "We will contact you back soon.<br>"
+                + "TwoMates";
+
+        contentToUser = contentToUser.replace("[[name]]", contact.getName() + " " + contact.getLastName());
+        contentToUser = contentToUser.replace("[[message]]", contact.getMessage());
+        return contentToUser;
+    }
+
+    private static String getContentToCrew(Contact contact) {
+        String contentToCrew = "An user has sent a message <br>"
+                + "<p>[[username]]: </p>"
+                + "<p>[[message]]</p>"
+                + "<br><br><br>"
+                + "<p>Sent from: [[userEmail]]</p>";
+        contentToCrew = contentToCrew.replace("[[username]]", contact.getName() + " " + contact.getLastName());
+        contentToCrew = contentToCrew.replace("[[message]]", contact.getMessage());
+        contentToCrew = contentToCrew.replace("[[userEmail]]", contact.getEmail());
+        return contentToCrew;
+    }
 
 }
--- src/main/java/mates/web/wiki/services/MailService.java
+++ src/main/java/mates/web/wiki/services/MailService.java
@@ -1,4 +1,36 @@
 package mates.web.wiki.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
 public class MailService {
+    private final JavaMailSender mailSender;
+
+    private final String fromAddress = "hello@mates.sbs";
+    private final String senderName = "LandMates";
+
+    MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String toAddress, String subject, String content, boolean html) {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        try {
+            helper.setFrom(fromAddress, senderName);
+            helper.setTo(toAddress);
+            helper.setSubject(subject);
+            helper.setText(content, html);
+            mailSender.send(message);
+        } catch (UnsupportedEncodingException | MessagingException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
